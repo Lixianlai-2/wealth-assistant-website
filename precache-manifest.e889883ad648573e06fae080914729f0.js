f87e522f437e5f236cfe7151f4b911287a3c1226
MarkLee <1643565650@qq.com>
2022-06-04 10:03:38 +0800

    deploy

---
+++ precache-manifest.e889883ad648573e06fae080914729f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be95ffee6620e553d1e1",
+    "url": "/wealth-assistant-website/css/app.e4ec4cc5.css"
+  },
+  {
+    "revision": "764dc2281fedffb3671c6b5255040dfd",
+    "url": "/wealth-assistant-website/index.html"
+  },
+  {
+    "revision": "be95ffee6620e553d1e1",
+    "url": "/wealth-assistant-website/js/app.519d7c5c.js"
+  },
+  {
+    "revision": "b4f5a4ba0cd00d2b1bde",
+    "url": "/wealth-assistant-website/js/chunk-vendors.75b3d1f1.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/wealth-assistant-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wealth-assistant-website/robots.txt"
+  }
+]);
